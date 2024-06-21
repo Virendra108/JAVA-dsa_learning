6dc7a5d995e92cebc1cc64f8e28cd73db9501485
Virendra Ghule <113795394+Virendra108@users.noreply.github.com>
2024-06-21 23:50:44 +0530

    Create bit_basic.java

---
+++ 4-Bit_manipulation/bit_basic.java
@@ -0,0 +1,14 @@
+public class bit_basic{
+  public static void main(string[] args){
+    //BITWISE OPERATORS
+    
+    //Binary and  &
+    //Binary or   |
+    //Binary XOR  ^
+    //ones complement  ~
+    //left shift  <<
+    //right shift  >>
+    
+    
+  }
+}
