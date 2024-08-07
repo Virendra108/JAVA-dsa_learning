c7f8eeaf1deec031ee153e40412ecde6e926a695
Virendra Ghule <113795394+Virendra108@users.noreply.github.com>
2024-08-08 00:07:04 +0530

    Update GetSetThis.java

--- 5-Oops/GetSetThis.java
+++ 5-Oops/GetSetThis.java
@@ -1,17 +1,20 @@
 import java.util.*;
 
 public class GetSetThis {
-    Pen p1=new Pen();
-    p1.setcolor("Blue");
+    public static void main(String[] args) {
+        Pen p1=new Pen();
+        p1.setcolor("Blue");
+        System.out.println(p1.getcolor());
+    }
 }
 class Pen{
     private String color;
     private int tip;
-    String getcolor(){
+    String getcolor(){             //get function we use to fetch the data from class which is private or of toher acess modifier
         return this.color;
     }
-    void setcolor(String color){
-        this.color=color;
+    void setcolor(String color){   // samw way we cant modify rivate so we use set int that same class.
+        this.color=color;         // At here 'this' is important because there are two same named variables but when we used this.color-> refers to color of the class while the color int that function refers to parametre color.
     }
 
 }
